1b9e8ceeeb5294af801ba4042f200bfeb6b59623
Carlos Trujillo <krloslao90@gmail.com>
2023-05-03 00:30:37 -0400

    adding some server boilerplate (wip)

---
+++ server.js
@@ -0,0 +1,21 @@
+import usersDatabase from "./usersDatabase.js";
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("It Works!");
+});
+
+app.post("/signin", (req, res) => {
+	console.log(req.body);
+	res.send("DONE");
+});
+
+app.listen(port, () => {
+	console.log("Server is running on port 3000");
+});
